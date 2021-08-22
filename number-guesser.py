93331ca62b12495273e843f3dce72f17cd917dc7
Rodney Mills <mills.rj7@gmail.com>
2021-08-22 17:01:55 -0500

    init

--- number-guesser.py
+++ number-guesser.py
@@ -4,6 +4,7 @@ top_of_range = input("Type a number: ")
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
+    
 
     if top_of_range <= 0:
         print("Please type a number larger than 0 next time. ")
